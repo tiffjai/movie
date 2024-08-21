d6435b63407a978ec5f52297c0521ddeb49ebbfe
Stephanie <tiffanieho@outlook.com>
2024-08-21 18:54:26 +0100

    change to movie hall

--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -4,7 +4,7 @@ import PageWrapper from '../../components/PageWrapper';
 
 const HomePage = () => (
   <PageWrapper>
-    <h1>Welcome to the Movie Database</h1>
+    <h1>Welcome to the Movie Hall</h1>
     <p>Explore our collection of movies and vote for your favorites!</p>
   </PageWrapper>
 );
